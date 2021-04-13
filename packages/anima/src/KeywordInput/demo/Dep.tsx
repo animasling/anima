cc8c9a27eb7ab04884b5466055b086e6172d5861
animasling <animas@126.com>
2021-04-13 16:44:37 +0800

    feat: add demo

---
+++ packages/anima/src/KeywordInput/demo/Dep.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Input } from 'antd';
+
+const Dep: React.FC<{}> = () => {
+  return <Input />;
+};
+
+export default Dep;
--- packages/anima/src/KeywordInput/demo/basic.tsx
+++ packages/anima/src/KeywordInput/demo/basic.tsx
@@ -1,80 +1,10 @@
 import React from 'react';
 import { Form } from 'antd';
 import { KeywordInput } from 'anima';
+import { formItemLayout, KEYWORD_MATCH_RULES } from './constants';
+import Dep from './Dep';
 
 const FormItem = Form.Item;
-
-const formItemLayout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    span: 16,
-  },
-};
-
-export const KEYWORD_MATCH_RULES = [
-  {
-    label: 'msg 或者 stack 匹配整个短语："hello world"',
-    value: 'hello world',
-  },
-  {
-    label: 'msg 匹配 hello 或者 world',
-    value: 'msg:hello world',
-  },
-  {
-    label: 'msg 或者 stack 包含 value1 和 value2',
-    value: 'value1 AND value2',
-  },
-  {
-    label: 'msg 或者 stack 包含 value1 或 value2',
-    value: 'value1 OR value2',
-  },
-  {
-    label: 'msg 或者 stack 不包含 value',
-    value: 'NOT value',
-  },
-  {
-    label: 'msg 或者 stack 必须包含 value1，以及 value2、value3 中的任一个',
-    value: 'value1 AND (value2 OR value3)',
-  },
-  {
-    label: '日志类名为：com.ymm.service.impl.*ServiceImpl 的日志',
-    value: 'loc:com.ymm.service.impl.*ServiceImpl',
-  },
-  {
-    label: '线程名称为：catalina-exec-21 的日志',
-    value: 'thread:catalina-exec-21',
-  },
-  {
-    label: '链路ID为：xxxxxx 的日志',
-    value: 'sid:xxxxxx',
-  },
-  {
-    label: '根链路ID为：xxxxxx 的日志',
-    value: 'tid:xxxxxx',
-  },
-  {
-    label: 'msg 匹配 "hello world" 并且链路ID为 xxxxxx 的日志',
-    value: 'msg:"hello world" AND sid:"xxxxxx"',
-  },
-  {
-    label: 'throwable为：xxxxxx的日志',
-    value: 'throwable:xxxxxx',
-  },
-  {
-    label: 'uid为：xxxxxx 的日志',
-    value: 'uid:xxxxxx',
-  },
-  {
-    label: 'oid为：xxxxxx 的日志',
-    value: 'oid:xxxxxx',
-  },
-  {
-    label: 'tel为：xxxxxx 的日志',
-    value: 'tel:xxxxxx',
-  },
-];
 
 const KeywordInputTest: React.FC<{}> = () => {
   const [form] = Form.useForm();
@@ -102,6 +32,7 @@ const KeywordInputTest: React.FC<{}> = () => {
           onChange={handleQueryChange}
           onOk={handleQueryModalOK}
         />
+        <Dep />
       </FormItem>
     </Form>
   );
